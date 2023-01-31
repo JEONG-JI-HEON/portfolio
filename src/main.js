851a6ac12bbe791c9064427cb04576fd072baff8
JEONG-JI-HEON <wlgjs0804@naver.com>
2023-01-31 23:00:48 +0900

    font파일 추가 및 main.css에 적용

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from "./App.vue";
 import store from "./store";
 import Vue3Lottie from "vue3-lottie";
 import "vue3-lottie/dist/style.css";
+import "@/assets/css/main.css";
 
 createApp(App).use(store).use(Vue3Lottie).mount("#app");
